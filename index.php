688d3ce0e8b8f06ce21e98473b1de70aafee5ff5
sarvesh <sarveshagrawal@karunya.edu.in>
2020-06-06 23:59:05 +0530

    new index.php

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
- <!DOCTYPE html>
-<html>
-<body>
 
 <?php
-echo "Project Done by :-";
-echo "Akul Maurya";
-echo "Sarvesh Agrawal";
-echo "Swapnil Lader";
-?>
 
+echo "
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DEVOPS AUTOMATION</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>
+</head>
+<body style='background-color: black; color: white;'>
+
+	<center>
+			<div style='width: 70%; padding: 50px;'>
+				<h2 style='border:2px solid khaki; padding: 8px;'>Hello World</h2>
+				<h3 style='border:2px solid Tomato;  padding: 8px;'>This Project is Done By</h3>
+				<h3 style='border:2px solid aqua; padding: 8px;'>-: Tech Boutique Team :-</h3>
+				<h3 style='border:2px solid white; padding: 8px;'>Akul Maurya</h3>
+				<h3 style='border:2px solid green; padding: 8px;'>Swapnil Lader</h3>
+				<h3 style='border:2px solid orange; padding: 8px;'>Sarvesh Agrawal</h3>
+
+
+				<p style='border:2px solid purple;  padding: 8px;'>
+				Rollout Update with 0 Downtime.
+				</p>
+
+			</div>
+	</center>
+	
 </body>
-</html> 
+</html>
+"
+?>
